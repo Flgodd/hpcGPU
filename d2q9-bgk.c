5004d9f0f6d7aeb2fae4750eb8326d350e2bcf5b
Flgodd <benjyaram@gmail.com>
2024-04-24 19:52:46 +0100

    setup

--- d2q9-bgk.c
+++ d2q9-bgk.c
@@ -264,9 +264,9 @@ int timestep(const t_param params, t_speed* cells, t_speed* tmp_cells, int* obst
   //rebound(params, cells, tmp_cells, obstacles, ocl);
   //collision(params, cells, tmp_cells, obstacles, ocl);
 
-//  cl_mem temp = ocl.cells;
-//  ocl.cells = ocl.tmp_cells;
-//  ocl.tmp_cells = temp;
+  cl_mem temp = ocl.cells;
+  ocl.cells = ocl.tmp_cells;
+  ocl.tmp_cells = temp;
 
   return t;
 }
@@ -276,14 +276,16 @@ int accelerate_flow(const t_param params, t_speed* cells, int* obstacles, t_ocl 
   cl_int err;
 
   // Set kernel arguments
-  if(flip == 0) {
+  /*if(flip == 0) {
       err = clSetKernelArg(ocl.accelerate_flow, 0, sizeof(cl_mem), &ocl.cells);
       checkError(err, "setting accelerate_flow arg 0", __LINE__);
   }
   if(flip == 1){
       err = clSetKernelArg(ocl.accelerate_flow, 0, sizeof(cl_mem), &ocl.tmp_cells);
       checkError(err, "setting accelerate_flow arg 0", __LINE__);
-  }
+  }*/
+    err = clSetKernelArg(ocl.accelerate_flow, 0, sizeof(cl_mem), &ocl.cells);
+    checkError(err, "setting accelerate_flow arg 0", __LINE__);
   err = clSetKernelArg(ocl.accelerate_flow, 1, sizeof(cl_mem), &ocl.obstacles);
   checkError(err, "setting accelerate_flow arg 1", __LINE__);
   err = clSetKernelArg(ocl.accelerate_flow, 2, sizeof(cl_int), &params.nx);
@@ -313,9 +315,9 @@ float propagate(const t_param params, t_speed* cells, t_speed* tmp_cells, t_ocl 
   cl_int err;
 
   // Set kernel arguments
-  err = clSetKernelArg(ocl.propagate, flip, sizeof(cl_mem), &ocl.cells);
+  err = clSetKernelArg(ocl.propagate, 0, sizeof(cl_mem), &ocl.cells);
   checkError(err, "setting propagate arg 0", __LINE__);
-  err = clSetKernelArg(ocl.propagate, !flip, sizeof(cl_mem), &ocl.tmp_cells);
+  err = clSetKernelArg(ocl.propagate, 1, sizeof(cl_mem), &ocl.tmp_cells);
   checkError(err, "setting propagate arg 1", __LINE__);
   err = clSetKernelArg(ocl.propagate, 2, sizeof(cl_mem), &ocl.obstacles);
   checkError(err, "setting propagate arg 2", __LINE__);
