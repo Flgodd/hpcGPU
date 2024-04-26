d9e9bfca3f8836c50fbf5f9a8b5e1734830c2362
Flgodd <benjyaram@gmail.com>
2024-04-26 15:21:34 +0100

    cc red

--- run.sh
+++ run.sh
@@ -23,7 +23,7 @@ module load libs/cuda/10.0-gcc-5.4.0-2.26
 
 
 #! Run the executable
-ncu ./d2q9-bgk input_128x128.params obstacles_128x128.dat
+./d2q9-bgk input_128x128.params obstacles_128x128.dat
 #./d2q9-bgk input_128x256.params obstacles_128x256.dat
 #./d2q9-bgk input_256x256.params obstacles_256x256.dat
 #./d2q9-bgk input_1024x1024.params obstacles_1024x1024.dat
