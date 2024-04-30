58545dcad34ffddc6a07f376319bcdc9270d8736
Flgodd <benjyaram@gmail.com>
2024-04-30 19:13:31 +0100

    perf

--- run.sh
+++ run.sh
@@ -28,6 +28,6 @@ module use /software/x86/tools/nvidia/hpc_sdk/modulefiles
 #./d2q9-bgk input_128x128.params obstacles_128x128.dat
 #./d2q9-bgk input_256x256.params obstacles_256x256.dat
 #./d2q9-bgk input_1024x1024.params obstacles_1024x1024.dat
-./d2q9-bgk input_2048x2048.params obstacles_2048x2048.dat
+perf stat -e ./d2q9-bgk input_2048x2048.params obstacles_2048x2048.dat
 #cd /user/home/tn21145/osu-micro-benchmarks-7.4/c/mpi/pt2pt/standard/
 #srun --mpi=pmi2 -N 1 --ntasks-per-node=2 ./osu_latency
